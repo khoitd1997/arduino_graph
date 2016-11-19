9f3bec08707d32685491058a4af802bbe2785348
khoitd1997 <kdtrinh1@uci.edu>
2016-11-18 23:59:51 -0800

    Major change involving safer matrix governance
    The new matrix can store maximum of 10000 run(can be increased), graph
    cosmetics are changed

--- arduino_graph.m
+++ arduino_graph.m
@@ -1,6 +1,8 @@
 %% Initial setup
 se=instrhwinfo('serial');%find the available port on tbe computer, run again if the usb is disconnected
-d=[];
+%d=[]; %empty array slows down data collection 
+d=ones(10000,2); %capable of storing 10000 run change as you need, limited only by ram of the computer
+count=1;
 if isempty(se.AvailableSerialPorts())
     return
 else
@@ -10,23 +12,28 @@ else
     fopen(port);
     readasync(port);
     see=instrhwinfo('serial');
-%% loop start here, loop until the arduino is unplugged
+%% loop start here, loop until the arduino is unplugged(try bluetooth see if it works)
     while isempty(see.SerialPorts)~=1%check if there is any arduino plugged in
     a=fscanf(port);
     b=textscan(a,'%d');% %d is working fine but not for integer, try %f
-    d=[d;b{1}(1) b{1}(2)];%build a matrix of data
-    app.NumericEditField2.Value=b{1}(2);
-    plot(b{1}(1),b{1}(2),'.k');
+    
+    %build a matrix of data
+    d(count,1)=b{1}(1);
+    d(count,2)=b{1}(2);
+    
+    plot(b{1}(1),b{1}(2),'-*k');
     pause(20/1000)
     see=instrhwinfo('serial');
+    count=count+1;
     end
 %% cleaning up
     stopasync(port);
     fclose(port);
-    file_name=[strrep(datestr(now),':','`'),'.xlsm'];
+    file_name=[strrep(datestr(now),':','`'),'.xlsm']; 
+    d(count:end,:)=[];%truncating the matrix from row number count to end
     xlswrite(file_name,d);%work but a little slow, tested with a small maxtrix, about the same time as writetable but no header
     clc
-    clear a b se
+    clear a b se count d 
     clear instrfind
     hold off
     %fclose(instrfind) will mostly fix any problems, use when u r doomed
